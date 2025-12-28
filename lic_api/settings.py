fa00e08bd59da1489f5b0179dffa4222bd58973d
AbuArwa001 <khalfanathman12@yahoo.com>
2025-12-29 01:15:28 +0300

    feat: Remove  from  and add  to .

--- lic_api/settings.py
+++ lic_api/settings.py
@@ -47,7 +47,6 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 ALLOWED_HOSTS = [
     '54.161.31.236',
     'diatomaceous-preventively-amber.ngrok-free.dev',
-    'langataislamiccenter.org',
     'localhost',
     '127.0.0.1',
     'langata-islamic-center.vercel.app',
@@ -177,6 +176,7 @@ CSRF_TRUSTED_ORIGINS = [
     "https://langataislamiccenter.org",
     "https://www.langataislamiccenter.org",
     "https://liwomasjid.co.ke",
+    "https://langata-islamic-center.vercel.app",
 ]
 # CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
